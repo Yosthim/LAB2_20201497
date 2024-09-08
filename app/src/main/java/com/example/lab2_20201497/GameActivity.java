f2835b6299740d187aa7b924f87cc435f5e5939d
Yosthim <a20201497@pucp.edu.pe>
2024-09-07 19:56:18 -0500

    Parte visual de la parte 2

--- app/src/main/java/com/example/lab2_20201497/GameActivity.java
+++ app/src/main/java/com/example/lab2_20201497/GameActivity.java
@@ -1,6 +1,17 @@
 package com.example.lab2_20201497;
 
+import android.os.Bundle;
+
+import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
 
 public class GameActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.game_activity);
+
+    }
 }
